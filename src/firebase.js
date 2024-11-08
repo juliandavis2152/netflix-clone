035e399a68931237c2176e7524d02cf5b4b01fcd
juliandavis2152 <juliandavis2152@gmail.com>
2024-11-07 21:37:12 -0700

    Remove .env.local file

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, si
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
-
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyAfbG91WpiXfQwzoLdplXTZvcgR6S7cPnk",
+    authDomain: "netflix-clone-362c1.firebaseapp.com",
+    projectId: "netflix-clone-362c1",
+    storageBucket: "netflix-clone-362c1.firebasestorage.app",
+    messagingSenderId: "424533823409",
+    appId: "1:424533823409:web:462b886ce4696d888d7790"
   };
 
 const app = initializeApp(firebaseConfig);
